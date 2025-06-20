c3889714e96484727771be90a567bedb8d3168a8
watunh <152104567+watunh@users.noreply.github.com>
2025-06-20 18:38:27 +0800

    Update UAS_MBD.js

---
+++ UAS_MBD.js
@@ -0,0 +1,17 @@
+// key_value
+{ "_id": 1, "nama": "Keyboard Mechanical", "harga": 350000, "stok": 25, "kategori": "Elektronik" }
+{ "_id": 2, "nama": "Mouse Wireless", "harga": 150000, "stok": 50, "kategori": "Elektronik" }
+{ "_id": 3, "nama": "Monitor 24 inch", "harga": 1750000, "stok": 15, "kategori": "Elektronik" }
+{ "_id": 4, "nama": "Printer Canon", "harga": 850000, "stok": 10, "kategori": "Elektronik" }
+{ "_id": 5, "nama": "Speaker Bluetooth", "harga": 300000, "stok": 30, "kategori": "Elektronik" }
+
+// wide_column
+{
+  "_id": 1,
+  "nama": "Keyboard Mechanical",
+  "harga": 350000,
+  "stok": 25,
+  "kategori": "Elektronik",
+  "merk": "Logitech",
+  "garansi": "1 tahun"
+}
